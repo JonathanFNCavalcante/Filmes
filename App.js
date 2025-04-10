90b71fdfb77bd97bec1601b141f35fc348873817
JonathanFNCavalcante <jonathanoe2112@gmail.com>
2025-04-10 08:58:24 -0300

    Atividade 2

--- App.js
+++ App.js
@@ -11,14 +11,38 @@ const Filmes = () => {
       vote: '7.316',
       duration: '112 min',
       releaseDate: '2023-07-19',
-      poster: 'https://br.web.img3.acsta.net/pictures/23/06/21/21/10/1335465.jpg',
+      poster: 'https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
       cast: [
-        { name: 'Margot Robbie', character: 'Barbie' },
-        { name: 'Ryan Gosling', character: 'Ken' },
-        { name: 'America Ferrera', character: 'Gloria' },
-        { name: 'Kate McKinnon', character: 'Barbie' },
-        { name: 'Ariana Greenblatt', character: 'Sasha' },
-        { name: 'Michael Cera', character: 'Allan' }
+        { 
+          name: 'Margot Robbie', 
+          character: 'Barbie',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTgxNDcwMzU2Nl5BMl5BanBnXkFtZTcwNDc4NzkzOQ@@._V1_UY1200_CR84,0,630,1200_AL_.jpg'
+        },
+        { 
+          name: 'Ryan Gosling', 
+          character: 'Ken',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_.jpg'
+        },
+        { 
+          name: 'America Ferrera', 
+          character: 'Gloria',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTY0Mzc3NzY4NF5BMl5BanBnXkFtZTcwMDYzOTIzMw@@._V1_.jpg'
+        },
+        { 
+          name: 'Kate McKinnon', 
+          character: 'Barbie',
+          image: 'https://m.media-amazon.com/images/M/MV5BMzY3ZWY4ZDQtODU5MC00MTFjLWExZDktZmVmNjVhOGZmNjdhXkEyXkFqcGdeQXVyMjQxMDQzMjA@._V1_.jpg'
+        },
+        { 
+          name: 'Ariana Greenblatt', 
+          character: 'Sasha',
+          image: 'https://m.media-amazon.com/images/M/MV5BNmUxNzRkYTItZjQ5MC00ZDEwLWE2ZDktYmJjZjE5NDA0MzBkXkEyXkFqcGdeQXVyMzAzNTY3MDM@._V1_.jpg'
+        },
+        { 
+          name: 'Michael Cera', 
+          character: 'Allan',
+          image: 'https://m.media-amazon.com/images/M/MV5BMjVlZGRhYjktNWY0MS00ZjY0LTlhNTEtNzQwY2ZiZWYyZWIyXkEyXkFqcGdeQXVyMTk4MDgwNA@@._V1_.jpg'
+        }
       ]
     },
     {
@@ -29,14 +53,38 @@ const Filmes = () => {
       vote: '8.124',
       duration: '180 min',
       releaseDate: '2023-07-20',
-      poster: 'https://upload.wikimedia.org/wikipedia/pt/5/5b/Oppenheimer_poster.jpg',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg',
       cast: [
-        { name: 'Cillian Murphy', character: 'J. Robert Oppenheimer' },
-        { name: 'Emily Blunt', character: 'Katherine Oppenheimer' },
-        { name: 'Matt Damon', character: 'Leslie Groves' },
-        { name: 'Robert Downey Jr.', character: 'Lewis Strauss' },
-        { name: 'Florence Pugh', character: 'Jean Tatlock' },
-        { name: 'Josh Hartnett', character: 'Ernest Lawrence' }
+        { 
+          name: 'Cillian Murphy', 
+          character: 'J. Robert Oppenheimer',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTUxMzU2MTk1OF5BMl5BanBnXkFtZTgwNDg0NjIwMDI@._V1_.jpg'
+        },
+        { 
+          name: 'Emily Blunt', 
+          character: 'Katherine Oppenheimer',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTY5ODcxMDU4NV5BMl5BanBnXkFtZTcwMjAzNjQyNQ@@._V1_.jpg'
+        },
+        { 
+          name: 'Matt Damon', 
+          character: 'Leslie Groves',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTM0NzYzNDgxMl5BMl5BanBnXkFtZTcwMDg2MTMyMw@@._V1_.jpg'
+        },
+        { 
+          name: 'Robert Downey Jr.', 
+          character: 'Lewis Strauss',
+          image: 'https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_.jpg'
+        },
+        { 
+          name: 'Florence Pugh', 
+          character: 'Jean Tatlock',
+          image: 'https://m.media-amazon.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTgwOTIxNjA0OTE@._V1_.jpg'
+        },
+        { 
+          name: 'Josh Hartnett', 
+          character: 'Ernest Lawrence',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTkzMzA5Nzk1Nl5BMl5BanBnXkFtZTYwNzQ1Mjgy._V1_.jpg'
+        }
       ]
     },
     {
@@ -47,14 +95,38 @@ const Filmes = () => {
       vote: '8.521',
       duration: '166 min',
       releaseDate: '2024-02-29',
-      poster: 'https://ingresso-a.akamaihd.net/prd/img/movie/duna-parte-2/3971d5d6-702d-40d8-b990-872d4ffe3e32.webp',
+      poster: 'https://m.media-amazon.com/images/M/MV5BODI0YjNhNjUtYjM0My00MTUwLWFlYTMtMWI2NGUzYjNjNDVlXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg',
       cast: [
-        { name: 'Timothée Chalamet', character: 'Paul Atreides' },
-        { name: 'Zendaya', character: 'Chani' },
-        { name: 'Rebecca Ferguson', character: 'Lady Jessica' },
-        { name: 'Josh Brolin', character: 'Gurney Halleck' },
-        { name: 'Javier Bardem', character: 'Stilgar' },
-        { name: 'Austin Butler', character: 'Feyd-Rautha Harkonnen' }
+        { 
+          name: 'Timothée Chalamet', 
+          character: 'Paul Atreides',
+          image: 'https://m.media-amazon.com/images/M/MV5BOWU1Nzg0M2ItYjEzMi00ODliLThkODAtNGEyYzRkZTBmMmEzXkEyXkFqcGdeQXVyNDk2MjQ4Mzk@._V1_.jpg'
+        },
+        { 
+          name: 'Zendaya', 
+          character: 'Chani',
+          image: 'https://m.media-amazon.com/images/M/MV5BMjAxZTk4NDAtYjI3Mi00OTk3LTg0NDEtNWFlNzE5NDM5MWM1XkEyXkFqcGdeQXVyOTI3MjYwOQ@@._V1_.jpg'
+        },
+        { 
+          name: 'Rebecca Ferguson', 
+          character: 'Lady Jessica',
+          image: 'https://m.media-amazon.com/images/M/MV5BNzA4NDA1MTA5NV5BMl5BanBnXkFtZTcwNDkyODM4OA@@._V1_.jpg'
+        },
+        { 
+          name: 'Josh Brolin', 
+          character: 'Gurney Halleck',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTQ1MzYyMjQ0Nl5BMl5BanBnXkFtZTcwMTA0ODkyMg@@._V1_.jpg'
+        },
+        { 
+          name: 'Javier Bardem', 
+          character: 'Stilgar',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTY1NTc4NTYzMF5BMl5BanBnXkFtZTcwMDI2NzI2Mg@@._V1_.jpg'
+        },
+        { 
+          name: 'Austin Butler', 
+          character: 'Feyd-Rautha Harkonnen',
+          image: 'https://m.media-amazon.com/images/M/MV5BZmNiZmMzN2MtOTNkMy00YjA1LTg4NzktMWI4Njg1NzI3ODAyXkEyXkFqcGdeQXVyMTMyNzQyMzky._V1_.jpg'
+        }
       ]
     },
     {
@@ -65,14 +137,38 @@ const Filmes = () => {
       vote: '7.935',
       duration: '141 min',
       releaseDate: '2023-12-22',
-      poster: 'https://static.wikia.nocookie.net/dublagem/images/3/3e/Pobres_Criaturas.jpg/revision/latest?cb=20240311033940&path-prefix=pt-br',
+      poster: 'https://m.media-amazon.com/images/M/MV5BNGIyYWMzNjktNDE3MC00YWQyLWEyMmEtN2ZmNzZhZDk3NGJlXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg',
       cast: [
-        { name: 'Emma Stone', character: 'Bella Baxter' },
-        { name: 'Mark Ruffalo', character: 'Duncan Wedderburn' },
-        { name: 'Willem Dafoe', character: 'Dr. Godwin Baxter' },
-        { name: 'Ramy Youssef', character: 'Max McCandles' },
-        { name: 'Jerrod Carmichael', character: 'Harry Astley' },
-        { name: 'Christopher Abbott', character: 'Simon Blenkinsopp' }
+        { 
+          name: 'Emma Stone', 
+          character: 'Bella Baxter',
+          image: 'https://m.media-amazon.com/images/M/MV5BMjI4NjM1NDkyN15BMl5BanBnXkFtZTgwODgyNTY1MjE@._V1_.jpg'
+        },
+        { 
+          name: 'Mark Ruffalo', 
+          character: 'Duncan Wedderburn',
+          image: 'https://m.media-amazon.com/images/M/MV5BNWIzZTI1ODUtZTUzMC00NTdiLWFlOTYtZTg4MGZkYmU4YzNjXkEyXkFqcGdeQXVyNTExOTk5Nzg@._V1_.jpg'
+        },
+        { 
+          name: 'Willem Dafoe', 
+          character: 'Dr. Godwin Baxter',
+          image: 'https://m.media-amazon.com/images/M/MV5BNmJkYzJkMmQtYzBlMi00YmMzLWFkMjYtYzRkYjkwZjNhOWYxXkEyXkFqcGdeQXVyMTI3MDk3MzQ@._V1_.jpg'
+        },
+        { 
+          name: 'Ramy Youssef', 
+          character: 'Max McCandles',
+          image: 'https://m.media-amazon.com/images/M/MV5BOTgzODAyODA1Ml5BMl5BanBnXkFtZTgwNzIzMTA0NzM@._V1_.jpg'
+        },
+        { 
+          name: 'Jerrod Carmichael', 
+          character: 'Harry Astley',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTk3MDQxNDM5MF5BMl5BanBnXkFtZTgwNDY4NjU3MjE@._V1_.jpg'
+        },
+        { 
+          name: 'Christopher Abbott', 
+          character: 'Simon Blenkinsopp',
+          image: 'https://m.media-amazon.com/images/M/MV5BZDM1MGQxMzktMTBkYS00MTY2LTk3NjItZTIwNjhhZDY3NjU4XkEyXkFqcGdeQXVyNTExOTk5Nzg@._V1_.jpg'
+        }
       ]
     },
     {
@@ -83,14 +179,38 @@ const Filmes = () => {
       vote: '7.210',
       duration: '116 min',
       releaseDate: '2023-12-15',
-      poster: 'https://upload.wikimedia.org/wikipedia/pt/d/db/Wonka_%28filme%29.jpg',
+      poster: 'https://m.media-amazon.com/images/M/MV5BNDM4NTk0NjktZGVmOC00YTUwLTk2YzgtZWVmMGQ5NTAyNjcwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg',
       cast: [
-        { name: 'Timothée Chalamet', character: 'Willy Wonka' },
-        { name: 'Olivia Colman', character: 'Mrs. Scrubitt' },
-        { name: 'Hugh Grant', character: 'Oompa Loompa' },
-        { name: 'Keegan-Michael Key', character: 'Chefe de Polícia' },
-        { name: 'Sally Hawkins', character: 'Mãe de Willy' },
-        { name: 'Rowan Atkinson', character: 'Padre Julius' }
+        { 
+          name: 'Timothée Chalamet', 
+          character: 'Willy Wonka',
+          image: 'https://m.media-amazon.com/images/M/MV5BOWU1Nzg0M2ItYjEzMi00ODliLThkODAtNGEyYzRkZTBmMmEzXkEyXkFqcGdeQXVyNDk2MjQ4Mzk@._V1_.jpg'
+        },
+        { 
+          name: 'Olivia Colman', 
+          character: 'Mrs. Scrubitt',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTUxODY0MjI4Ml5BMl5BanBnXkFtZTgwMTM0NTcwOTE@._V1_.jpg'
+        },
+        { 
+          name: 'Hugh Grant', 
+          character: 'Oompa Loompa',
+          image: 'https://m.media-amazon.com/images/M/MV5BMjA5ODgyNzcxMV5BMl5BanBnXkFtZTcwODc0MTUyMw@@._V1_.jpg'
+        },
+        { 
+          name: 'Keegan-Michael Key', 
+          character: 'Chefe de Polícia',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTcyMjM2MjIxMF5BMl5BanBnXkFtZTgwODE0MzE4NjE@._V1_.jpg'
+        },
+        { 
+          name: 'Sally Hawkins', 
+          character: 'Mãe de Willy',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTUxNzY3MjE0N15BMl5BanBnXkFtZTcwNzQwMzg5Ng@@._V1_.jpg'
+        },
+        { 
+          name: 'Rowan Atkinson', 
+          character: 'Padre Julius',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTg3NDUxMzQ0Ml5BMl5BanBnXkFtZTcwNzcyMDYwNA@@._V1_.jpg'
+        }
       ]
     }
   ];
@@ -108,7 +228,7 @@ const Filmes = () => {
       <ScrollView>
         <View style={styles.movieContainer}>
           <Image 
-            source={{ uri: 'https://upload.wikimedia.org/wikipedia/pt/d/db/Wonka_%28filme%29.jpg' }} 
+            source={{ uri: selectedMovie.poster }} 
             style={styles.posterImage} 
           />
           <View style={styles.movieInfo}>
@@ -128,7 +248,7 @@ const Filmes = () => {
           {selectedMovie.cast.map((actor, index) => (
             <TouchableOpacity key={index} style={styles.actorCard}>
               <Image 
-                source={{ uri: 'https://cdn-icons-png.flaticon.com/128/10829/10829389.png' }} 
+                source={{ uri: actor.image }} 
                 style={styles.actorImage} 
               />
               <View style={styles.actorInfo}>
@@ -149,7 +269,7 @@ const Filmes = () => {
               onPress={() => setSelectedMovie(movie)}
             >
               <Image 
-                source={{ uri: 'https://cdn-icons-png.flaticon.com/128/10829/10829389.png' }} 
+                source={{ uri: movie.poster }} 
                 style={styles.thumbnail} 
               />
               <Text style={styles.thumbnailTitle}>{movie.title}</Text>
@@ -210,6 +330,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginVertical: 12,
+    paddingHorizontal: 16,
   },
   actorCard: {
     flexDirection: 'row',
@@ -217,6 +338,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 12,
     marginBottom: 8,
+    marginHorizontal: 16,
     alignItems: 'center',
     elevation: 1,
   },
